88164bfc9fc3de991a7ce3e1df6bc64511a7a3e2
curtis <curtis@serverascode.com>
2011-04-29 12:46:01 -0600

    new reptile.py with class and removed README replaced with README.textile

--- reptile.py
+++ reptile.py
@@ -5,95 +5,87 @@ import sys, os, shutil
 from optparse import OptionParser
 from Cheetah.Template import Template
 
-VERSION = "0.1"
-TEMPLATE = "templates/reptile.tpl"
+VERSION = '0.1.1'
+TEMPLATE = 'templates/reptile.tpl'
 
-def create_site(directory):
+class Reptile:
+    
+    def __init__(self, template, in_directory, out_directory):
+        self.in_directory = in_directory
+        self.out_directory = out_directory
+        self.template = template
+        self.set_files(in_directory)
+        self.create_out_directory()
+        self.export_html()
 
-    dirs = os.listdir(directory)
-    files = []
-    # The point is this mumbo-jumbo is to only go down one level into the main dir
-    for dir in dirs:
-        dir_files = os.listdir(directory + "/" + dir)
-        for dir_file in dir_files:
-            if os.path.isfile(directory + "/" + dir + "/" + dir_file):
-                files.append(directory + "/" + dir + "/" + dir_file)
-                #print opts.directory + "/" + dir + "/" + dir_file
+    def set_files(self, directory):
+        dirs = os.listdir(directory)
+        files = []
+        # The point is this mumbo-jumbo is to only go down one level into the main dir
+        for dir in dirs:
+            dir_files = os.listdir(os.path.join(directory, dir))
+            for dir_file in dir_files:
+                if os.path.isfile(os.path.join(directory, dir, dir_file)):
+                    files.append(os.path.join(directory, dir, dir_file))
 
-    return files
+        self.files = files
 
-def main(args):
-
-    # Optional Args
-    parser = OptionParser(conflict_handler="resolve")
-    # Just do one file
-    parser.add_option("-f", "--file", dest="file")
-    # Use a directory, create site based on a directory structure
-    parser.add_option("-d", "--directory", dest="directory")
-    # Output html to another dir, requires --directory
-    parser.add_option("-o", "--output-directory", dest="output_directory")
-    (opts, args) = parser.parse_args()
-
-    if opts.directory and not opts.output_directory:
-        print "ERROR: If option --directory is set, then --output-directoy must be as well"
-        sys.exit(1)
-    elif opts.directory and not os.path.exists(opts.directory):
-        print "ERROR: Directory " + opts.directory + " does not exist"
-        sys.exit(1)
-    elif opts.directory:
-
-        files = create_site(opts.directory)
-
-        # Remove output_directory if it exists to make way for new output_dir
-        if os.path.isdir(opts.output_directory):
+    # FIXME
+    def create_out_directory(self):
+        if os.path.isdir(self.out_directory):
             try:
-                shutil.rmtree(opts.output_directory)
+                shutil.rmtree(self.out_directory)
             except:
-                print "ERROR: Could not remove " + opts.output_directory
+                print "ERROR: Could not remove " + self.out_directory
                 sys.exit(1)
-
-        # Create the dir now
+        
         try:
-            os.mkdir(opts.output_directory)
+            os.mkdir(self.out_directory)
         except:
-            print "ERROR: Could not create " + opts.output_directory
+            print "ERROR: Could not create " + self.out_directory
             sys.exit(1)
 
-        for input_textile_file in files:
-            dir, file = os.path.split(input_textile_file)
-            root, dir = os.path.split(dir)
-            # Change the output file name to filename.html instead of filename.textile
-            html_file_name, extension = os.path.splitext(file)
-            html_file_name = html_file_name + ".html"
-            output_html_file = opts.output_directory + "/" + dir + "/" + html_file_name
-            
-            # Create subdir
-            try:
-                os.mkdir(opts.output_directory + "/" + dir)
-            except:
-                print "ERROR: Could not create " + opts.output_directory + "/" + dir
+    # FIXME
+    def create_sub_directory(self, dir):
+        try:
+            os.mkdir(dir)
+        except:
+            print "ERROR: Could not create " + dir
 
-            if os.path.isfile(input_textile_file):   
-                f = open(input_textile_file)         
+    def export_html(self):
+
+        for file in self.files:
+            first_level_dir, file_name = os.path.split(file)
+            root_level_dir, first_level_dir = os.path.split(first_level_dir)
+            file_name, extension = os.path.splitext(file_name)
+            file_name = file_name + '.html'
+            output_html_file = os.path.join(self.out_directory, first_level_dir, file_name)
+
+            self.create_sub_directory(os.path.join(self.out_directory, first_level_dir))
+
+            if os.path.isfile(file):   
+                f = open(file)         
                 html = textile.textile(f.read())
                 textile_file = open(output_html_file, 'w')
-                #textile_file.write(html)
+                self.template.body = html
+                self.template.file_name = file_name
+                textile_file.write(self.template.respond())
+            
+def main(args):
 
-                t = Template(file=TEMPLATE)
-                t.body = html
-                t.file_name = html_file_name
-                textile_file.write(t.respond())
+    try:
+        # Make sure required args are set
+        sys.argv[1]
+        sys.argv[2]
+    except:
+        print "ERROR: Usage: " + str(sys.argv[0]) + " input_dir output_dir"
+        sys.exit(1)
+        
+    in_directory = sys.argv[1]
+    out_directory = sys.argv[2]
 
-            print output_html_file
-    else:
-        if opts.file:
-            f = open(opts.file, 'r')
-        else:
-            # Need to handle there being no input gracefully
-            f = sys.stdin
-        body = textile.textile(f.read())
-        print body
-
+    t = Template(file=TEMPLATE)
+    r = Reptile(t, in_directory, out_directory)
 
 if __name__ == '__main__':
     main(sys.argv)
