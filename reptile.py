ba8878adb752eaa02e07b69f7e7c02770333861b
curtis <curtis@serverascode.com>
2011-05-06 10:30:53 -0600

    added a few things to make this uglier than ever

--- reptile.py
+++ reptile.py
@@ -5,6 +5,7 @@ import sys, os, shutil, random
 from optparse import OptionParser
 from BeautifulSoup import BeautifulSoup
 from Cheetah.Template import Template
+from xml.sax.saxutils import escape
 
 VERSION = '0.1.1'
 HOME = '/home/curtis/working/reptile'
@@ -12,11 +13,16 @@ TEMPLATE = HOME + '/templates/reptile.tpl'
 # FIXME - bad name here
 CSS = HOME + '/css'
 
+
 class TextileFile:
     
     def __init__(self, path):
         self.path = path
         self.html = ''
+        self.TOC = ''
+        self.toc_list = []
+        self.toc_string = ''
+        self.HEADERS = ['h1', 'h2', 'h3', 'h4']
         # Contains {somerandomnumber: notextile_text}
         self.notextiles = {}
 
@@ -30,31 +36,72 @@ class TextileFile:
 
     def create_html(self):
         self.html = self.extract_notextile()
-        #print self.html
         self.html = textile.textile(self.html)
-        #print self.html
         self.html = self.replace_notextile()
-        #print self.html
+        self.TOC = self.create_toc()
+
+    def recurse_header(self,h):
+        try:
+            # If it doesn't have a name in headers, we skip it
+            if h.nextSibling.name in self.HEADERS:
+                last_header = h.nextSibling.name
+                self.toc_list.append(str(h.nextSibling))
+        except:
+            pass
+
+        try:
+            # if no nextSibling we're at the end, so quit
+            h.nextSibling
+        except:
+            return False
+
+        self.recurse_header(h.nextSibling)
+
+    def make_ordered_list_toc(self):
+        soup = BeautifulSoup(self.toc_string)
+        
+        # FIXME
+        #for h in soup.findAll():
+        #    print h
+            
+
+    def create_toc(self):
+        soup = BeautifulSoup(self.html)
+
+        # FIXME If there's no h1 this'll error out
+        for h in soup.findAll('h1'):
+
+            self.toc_list.append(str(h))
+            self.recurse_header(h)
+
+    
+        for i in self.toc_list:
+            self.toc_string = self.toc_string + '\n' + i
+
+        self.make_ordered_list_toc()
+
+        return self.toc_string
 
     def extract_notextile(self):
         soup = BeautifulSoup(self.text)
-        # obv this isn't going to work because pres will
-        # only be for one file and there will be many files...
         for idx, nt in enumerate(soup.findAll('notextile')):
             rand = str(random.random())
             # FIXME 
-            nt.replaceWith('\n\n' + rand + '\n\n')
+            nt.replaceWith(rand)
             self.notextiles[rand] = nt
             
-        return soup.prettify()
+        #return soup.prettify()
+        return soup.renderContents()
 
     def replace_notextile(self):
         soup = BeautifulSoup(self.html)
         for k, v in self.notextiles.items():
             for nt in soup.findAll(text=k):
+                # escape xml characters...
                 nt.replaceWith(v)
                
-        return soup.prettify()
+        #return soup.prettify()
+        return soup.renderContents()
 
 class Reptile:
     
@@ -129,9 +176,11 @@ class Reptile:
                 # Send the html as the "body" in the template
                 self.template.body = nt.html
                 self.template.file_name = file_name
+                self.template.toc = nt.TOC
                 # Finally write the template to the html file
                 html_file.write(self.template.respond())
- 
+
+
 def main(args):
 
     try:
